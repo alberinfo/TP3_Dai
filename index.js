910807d8d7061b997533e5f7a7b18bd04c6a3d91
Arotu <arotuort@gmail.com>
2023-04-28 09:00:42 -0300

    Create all pizza services

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-import config from './dbconfig.js';
-import sql from 'mssql';
+import PizzaService from './services/pizza-services.js'
+import pizza from './models/pizza.js'
 
-let pool = await sql.connect(config);
-let result = await pool.request().query("SELECT TOP 2 * FROM Pizza");
-
-console.log(result.recordsets.length);
-console.log(result.recordsets[0].length);
-console.log(result.recordsets[0]);
-console.log(result.recordset);
-console.log(result.returnValue);
-console.log(result.output);
-console.log(result.rowsAffected);
+let servicio = new PizzaService();
+console.log(await servicio.getAllPizzas());
+console.log("\n");
+console.log(await servicio.getPizzaById(2));
+console.log("\n");
+let pizzaParaInsertar = new pizza();
+pizzaParaInsertar.nombre = "prueba"; pizzaParaInsertar.libreDeGluten = false; pizzaParaInsertar.precio = 400; pizzaParaInsertar.descripcion = "AAAAAAAAAAA";
+console.log(await servicio.createPizza(pizzaParaInsertar));
+console.log(await servicio.getAllPizzas());
+console.log("\n");
 
 process.exit();
--- services/pizza-services.js
+++ services/pizza-services.js
@@ -1,10 +1,31 @@
-class PizzaService { 
-    getById = async (id) => {
+import pizza from '../models/pizza.js'
+import config from '../dbconfig.js';
+import sql from 'mssql';
+
+class PizzaService {
+
+    constructor() {
+        //this.pool = this.sqlConnectionWaiter();
+        //console.log(this.pool);
+    }
+
+    getAllPizzas = async () => {
+        let returnEntity = null;
+        try {
+            let connection = await sql.connect(config); //Esto es un asco. no se hace asi, pero javascript se queja si se hace de cualquier otra manera
+            let result = await connection.request().query("SELECT * FROM Pizza");
+            returnEntity = result.recordsets[0];
+        } catch(error) {
+            console.log(error);
+        }
+        return returnEntity;
+    }
+
+    getPizzaById = async (id) => {
         let returnEntity = null; 
-        console.log('Estoy en: PizzaService.GetById(id)');
-        try { 
-            let pool = await sql.connect(config);
-            let result = await pool.request()
+        try {
+            let connection = await sql.connect(config); //Esto es un asco. no se hace asi, pero javascript se queja si se hace de cualquier otra manera
+            let result = await connection.request()
                                 .input('pId', sql.Int, id)
                                 .query('SELECT * FROM Pizza WHERE id = @pId')
             returnEntity = result.recordsets[0][0];
@@ -13,4 +34,56 @@ class PizzaService {
         }
         return returnEntity;
     }
+
+    createPizza = async (Pizza) => {
+        let returnEntity = null;
+        try {
+            let connection = await sql.connect(config); //Esto es un asco. no se hace asi, pero javascript se queja si se hace de cualquier otra manera
+            let result = await connection.request()
+                                .input('pId', sql.Int, Pizza.id)
+                                .input('pNombre', sql.VarChar, Pizza.nombre)
+                                .input('pLibre', sql.Bit, Pizza.libreDeGluten)
+                                .input('pPrecio', sql.Float, Pizza.precio)
+                                .input('pDesc', sql.VarChar, Pizza.descripcion)
+                                .query("INSERT INTO Pizza (nombre, libreDeGluten, precio, descripcion) VALUES(@pNombre, @pLibre, @pPrecio, @pDesc)");
+            returnEntity = result; //Rows affected
+        } catch (error) {
+            console.log(error);
+        }
+        return returnEntity;
+    }
+
+    updatePizza = async (Pizza) => {
+        let returnEntity = null;
+        try {
+            let connection = await sql.connect(config); //Esto es un asco. no se hace asi, pero javascript se queja si se hace de cualquier otra manera
+            let result = await connection.request()
+                                .input('pId', sql.Int, Pizza.id)
+                                .input('pNombre', sql.VarChar, Pizza.nombre)
+                                .input('pLibre', sql.Bit, Pizza.libreDeGluten)
+                                .input('pPrecio', sql.Float, Pizza.precio)
+                                .input('pDesc', sql.VarChar, Pizza.descripcion)
+                                .query("UPDATE Pizza SET nombre = @pNombre, libreDeGluten = @pLibre, precio = @pPrecio, descripcion = @pDesc WHERE id = @pId");
+            returnEntity = result; //Rows affected
+        } catch (error) {
+            console.log(error);
+        }
+        return returnEntity;
+    }
+
+    deletePizza = async (id) => {
+        let returnEntity = null;
+        try {
+            let connection = await sql.connect(config); //Esto es un asco. no se hace asi, pero javascript se queja si se hace de cualquier otra manera
+            let result = await connection.request()
+                                .input('pId', sql.Int, Pizza.id)
+                                .query("DELETE FROM Pizza WHERE id = @pId");
+            returnEntity = result; //Rows affected
+        } catch (error) {
+            console.log(error);
+        }
+        return returnEntity;
+    }
 }
+
+export default PizzaService;
